d6e3ea810f67d632b3976b80bb96ea5290124190
EdkUnr <richardlvov1780@gmail.com>
2024-02-19 12:54:15 +1000

    Homework Theme "Collections part 1"

---
+++ src/main/java/com/empl/skyprohomeworkempl/Employee.java
@@ -0,0 +1,61 @@
+package com.empl.skyprohomeworkempl;
+
+import java.util.Objects;
+
+public class Employee {private String firstName;
+    private String lastName;
+    private double salary;
+    private int department;
+
+    public Employee(String fullName, double salary, int department) {
+        String[] parts = fullName.split("\\s+");
+        this.firstName = parts[0];
+        this.lastName = parts[1];
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public Employee(String firstName, String lastName) {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Employee employee = (Employee) obj;
+        return Objects.equals(firstName, employee.firstName) &&
+                Objects.equals(lastName, employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                ", department=" + department +
+                '}';
+    }
+}
+
