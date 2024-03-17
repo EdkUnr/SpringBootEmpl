f54f53d74a59967ac715c8a115984afaadaca914
richa <richardlvov1780@gmail.com>
2024-03-17 17:15:52 +1000

    Homework "Library"

---
+++ src/test/java/com/empl/skyprohomeworkempl/SkyproHomeworkEmplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empl.skyprohomeworkempl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproHomeworkEmplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
